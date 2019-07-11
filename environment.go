1a422e598d8248f56e2857f2f9ca40f8a563b8e7
Moad, Karl <karl.moad@gmail.com>
2019-07-11 13:45:57 -0400

    dev

--- environment.go
+++ environment.go
@@ -1,25 +1,46 @@
 package environment
 
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
 type EnvironmentalContext struct {
+	nickname2DbMap map[string]string
 	databases map[string]*databaseContext
 	objects 	map[string]interface{}
 }
 
 func InitEnvironmentalContext() *EnvironmentalContext {
-	return &EnvironmentalContext{make(map[string]*databaseContext), make(map[string]interface{})}
+	return &EnvironmentalContext{nickname2DbMap:make(map[string]string),
+							databases:make(map[string]*databaseContext),
+							objects:make(map[string]interface{})}
 }
 
 func(e *EnvironmentalContext) ConnectDatabase(nickname, driverName, connectionInfo string) error {
-	dc, err := initDatabaseContext(driverName,connectionInfo)
-	if err != nil {
-		return err
+
+	bytes := []byte(driverName+connectionInfo)
+	b := sha256.Sum256(bytes)
+	connInfoHash := hex.EncodeToString(b[:])
+
+	if _, ok := e.databases[connInfoHash]; !ok {
+		dc, err := initDatabaseContext(driverName,connectionInfo)
+		if err != nil {
+			return err
+		}
+		e.databases[connInfoHash] = dc
 	}
-	e.databases[nickname] = dc
+
+	e.nickname2DbMap[nickname] = connInfoHash
+
 	return nil
 }
 
 func(e *EnvironmentalContext) GetConnection(nickname string) *databaseContext {
-	return e.databases[nickname]
+	if val, ok := e.nickname2DbMap[nickname]; ok {
+		return e.databases[val]
+	}
+	return nil
 }
 
 func (e *EnvironmentalContext) Put(key string, val interface{}) error {
